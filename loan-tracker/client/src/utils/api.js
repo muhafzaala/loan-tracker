81b055c97695027ce015dc7474a91bd11d784c42
muhafzaala <mafzaala333@gmail.com>
2026-04-30 13:19:32 +0500

    update api url to railway

--- loan-tracker/client/src/utils/api.js
+++ loan-tracker/client/src/utils/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const apiClient = axios.create({
-  baseURL: 'http://localhost:5000/api',
+  baseURL: 'https://your-railway-url.up.railway.app/api',
   headers: { 'Content-Type': 'application/json' },
 });
 
